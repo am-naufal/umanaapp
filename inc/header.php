00b35f6b5302eec4dfe930d0baeeeb8ebae394ed
Zeinuri <zeinuriahmed33@gmail.com>
2022-07-28 13:46:03 +0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
-            <h6 class="font-weight-bolder text-white mb-0">Sistem Informasi Kehadirans Umana'</h6>
+            <h6 class="font-weight-bolder text-white mb-0">Sistem Informasi Kehadiran Umana'</h6>
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                 <li class="breadcrumb-item text-sm text-white active" aria-current="page">Pondok Pesantren Salafiyah Syafi'iyah Sukorejo Situbondo</li>
             </ol>
