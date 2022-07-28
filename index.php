95acacfb49249b4d525f939467af5abf93b5b12b
A. Muzacky Naufal Ammar <naufalahmad945@gmail.com>
2022-07-28 18:08:13 +0700

    update index

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-echo "Masuk";
+// include_once('pages/login/sign-in.php');
+
+include "./inc/koneksi.php";
+session_start();
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'pages/login/sing-in.php';
+header("Location: http://$host$uri/$extra");
+// if (!empty($_SESSION['id_user'])) {
+
+// } else {
+//     if (file_exists('./aksi/login.php')) {
+//         include "./aksi/absen.php";
+//     } else {
+//         echo "Konfigurasi tidak berhasil";
+//     }
+// }
