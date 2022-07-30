dc14f46e5cfcf74c2123aa51393e7bbdd5e5e927
A. Muzacky Naufal Ammar <naufalahmad945@gmail.com>
2022-07-30 10:15:32 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-include_once "../../inc/koneksi.php";
 session_start();
 $host = $_SERVER['HTTP_HOST'];
 $host .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
@@ -7,23 +6,14 @@ if ($_SESSION['status_login'] == 'true') {
     if ($_SESSION['level'] == 'admin') {
         // buka dashboard admin
         header('location: http://' . $host . 'home/home.php');
-    } elseif ($_SESSION['level'] == 'umana') {
+    } else if ($_SESSION['level'] == 'umana') {
         // buka acsess umana 
         header('location: http://' . $host . 'pages/login/absen.php');
+    } else {
+        header('location: http://' . $host . 'pages/login/sign-in.php');
     }
 } else {
     //buka page login / sing-up
+
     header('location: http://' . $host . 'pages/login/sign-in.php');
 }
-
-?>
-
-<!-- // if (!empty($_SESSION['id_user'])) {
-
-// } else {
-// if (file_exists('./aksi/login.php')) {
-// include "./aksi/absen.php";
-// } else {
-// echo "Konfigurasi tidak berhasil";
-// }
-// } -->
