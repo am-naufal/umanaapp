a03eeb492ebe2a6ca87988e3f47b2fe0314f6958
Aliwahyudi02 <aliwahyudi026@gmail.com>
2022-07-31 16:40:49 +0700

    Update form_instansi.php

--- pages/instansi/form_instansi.php
+++ pages/instansi/form_instansi.php
@@ -2,7 +2,7 @@
 <?php include "./../inc/sidebar.php"; ?>
 
 <!-- End Navbar -->
-<div class="card sadow-lg mx-4 card-profile-bottom">
+<div class="card shadow-lg mx-4 card-profile-bottom">
     <div class="card-body p-3">
         <div class="row gx-4">
             <div class="col-auto">
