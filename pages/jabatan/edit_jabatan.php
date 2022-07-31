7d2f20d418daacf1de3df6db22403efe152dd804
nailur02 <muhammadnailurrohman02@gmail.com>
2022-07-31 14:06:18 +0700

    Update edit_jabatan.php

--- pages/jabatan/edit_jabatan.php
+++ pages/jabatan/edit_jabatan.php
@@ -1,1 +1,116 @@
-<?php echo"ada" ?>
+<?php include "./../inc/koneksi.php"; ?>
+<?php include "./../inc/sidebar.php"; ?>
+<!-- End Navbar -->
+<div class="card shadow-lg mx-4 card-profile-bottom">
+    <div class="card-body p-3">
+        <div class="row gx-4">
+            <div class="col-auto">
+            </div>
+            <div class="col-auto my-auto">
+                <div class="h-100">
+                    <h5 class="mb-1">
+                        Edit Jabatan
+                    </h5>
+                    <p class="mb-0 font-weight-bold text-sm">
+                        PP. Salafiyah Syafi'iyah Sukorejo
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
+                <div class="nav-wrapper position-relative end-0">
+                    <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link mb-0 px-0 py-1 active d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="true">
+                                <i class="ni ni-app"></i>
+                                <span class="ms-2">App</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
+                                <i class="ni ni-email-83"></i>
+                                <span class="ms-2">Messages</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
+                                <i class="ni ni-settings-gear-65"></i>
+                                <span class="ms-2">Settings</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- form umana -->
+<div class="container-fluid py-4">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header pb-0">
+                </div>
+                <?php include "./../inc/koneksi.php";
+                $tampil = $koneksi->query("SELECT * FROM tb_jabatan WHERE kd_jabatan='$_GET[id]'");
+                $r = $tampil->fetch_object();
+                ?>
+                <form class="card-body" method="post" action="">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="example-text-input" class="form-control-label">Kode Jabatan<span class="help"> e.x. "12345678"</span></label>
+                                <input type="text" id="" name="kd_jabatan" class="form-control" value="<?php echo $r->kd_jabatan; ?>" placeholder="Kode Jabatan" readonly>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="example-text-input" class="form-control-label">Nama Jabatan<span class="help"> e.x. "Guru"</label>
+                                <input type="text" id="" name="nama_jabatan" class="form-control" value="<?php echo $r->nama_jabatan; ?>" placeholder="Jabatan">
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="horizontal dark">
+                    <button type="submit" name="simpan" value="simpan" class="btn btn-primary btn-sm ms-auto align-items-center">Simpan</button>
+                    <a type="submit" name="simpan" value="simpan" class="btn btn-warning btn-sm ms-auto align-items-center" href="tabel_jabatan.php">Kembali</a>
+                </form>
+                <?php
+                if (@$_POST['simpan']) {
+                    $query = $koneksi->query("UPDATE tb_jabatan SET nama_jabatan='$_POST[nama_jabatan]' 
+                                                        WHERE kd_jabatan='$_POST[kd_jabatan]'");
+                    if ($query) {
+                ?>
+                        <script>
+                            window.location.href = 'tabel_jabatan.php';
+                        </script>
+                <?php
+                    } else {
+                        echo "Data gagal disimpan";
+                    }
+                }
+                ?>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-profile">
+                <img src="../assets/img/kantor pusat.jpg" alt="Image placeholder" class="card-img-top">
+                <div class="card-body pt-0">
+                    <div class="text-center mt-4">
+                        <h5>
+                            <span class="font-weight-light">Alamat </span>Sekretariat
+                        </h5>
+                        <div class="h6 font-weight-300">
+                            <i class="ni location_pin mr-2"></i>Kantor Pusat PP. Salafiyah Syafi'iyah Sukorejo
+                        </div>
+                        <div class="h6 mt-4">
+                            <i class="ni business_briefcase-24 mr-2"></i>Jl. KHR. Syamsul Arifin, Sukorejo, Sumberejo, Kec. Banyuputih
+                        </div>
+                        <div>
+                            <i class="ni education_hat mr-2"></i>Kabupaten Situbondo, Jawa Timur 68374
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "./../inc/footer.php"; ?>
+</div>
