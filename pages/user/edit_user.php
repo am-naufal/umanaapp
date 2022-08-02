72020adcfc06a5ab677cea10a6c6fc0040289541
Zeinuri <92680451+Zeinuri@users.noreply.github.com>
2022-08-02 21:37:56 +0700

    Update edit_user.php

--- pages/user/edit_user.php
+++ pages/user/edit_user.php
@@ -17,30 +17,6 @@ $db = __database();
                     </p>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
-                <div class="nav-wrapper position-relative end-0">
-                    <ul class="nav nav-pills nav-fill p-1" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link mb-0 px-0 py-1 active d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="true">
-                                <i class="ni ni-app"></i>
-                                <span class="ms-2">App</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
-                                <i class="ni ni-email-83"></i>
-                                <span class="ms-2">Messages</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
-                                <i class="ni ni-settings-gear-65"></i>
-                                <span class="ms-2">Settings</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
         </div>
     </div>
 </div>
@@ -139,7 +115,7 @@ $db = __database();
         </div>
         <div class="col-md-4">
             <div class="card card-profile">
-                <img src="../assets/img/kantor pusat.jpg" alt="Image placeholder" class="card-img-top">
+                <img src="../../assets/img/kantor pusat.jpg" alt="Image placeholder" class="card-img-top">
                 <div class="card-body pt-0">
                     <div class="text-center mt-4">
                         <h5>
