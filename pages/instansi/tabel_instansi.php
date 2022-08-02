3e605ddbb99178f60ba8d3e0a09d8eb8e16a185c
Aliwahyudi02 <aliwahyudi026@gmail.com>
2022-08-02 22:02:34 +0700

    Update tabel_instansi.php

--- pages/instansi/tabel_instansi.php
+++ pages/instansi/tabel_instansi.php
@@ -28,6 +28,7 @@
                             <tbody>
                                 <?php
                                 include "./../inc/koneksi.php";
+                                $tampil = $koneksi->query("SELECT * FROM tb_instansi");
                                 $no = 1;
                                 while ($r = $tampil->fetch_array()) {
                                 ?>
