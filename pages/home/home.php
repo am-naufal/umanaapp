f8f096d95838f743338ae6b6893188624dfe5d63
Zeinuri <92680451+Zeinuri@users.noreply.github.com>
2022-08-02 13:33:21 +0700

    Update home.php

--- pages/home/home.php
+++ pages/home/home.php
@@ -168,31 +168,40 @@
             <div class="card card-carousel overflow-hidden h-100 p-0">
                 <div id="carouselExampleCaptions" class="carousel slide h-100" data-bs-ride="carousel">
                     <div class="carousel-inner border-radius-lg h-100">
-                        <div class="carousel-item h-100 active" style="background-image: url('../../assets/img/carousel-1.jpg');background-size: cover;">
+                        <div class="carousel-item h-100 active" style="background-image: url('../../assets/img/sukorejo.jpg');background-size: cover;">
                             <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
                                 <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
                                     <i class="ni ni-camera-compact text-dark opacity-10"></i>
                                 </div>
-                                <h5 class="text-white mb-1">Get started with Argon</h5>
-                                <p>There’s nothing I really wanted to do in life that I wasn’t able to get good at.</p>
+                                <h5 class="text-white mb-1">"Mondhuk Entar Ngabdhi ben Ngajhi"</h5>
+                                <p>KHR. Syamsul Arifin</p>
                             </div>
                         </div>
-                        <div class="carousel-item h-100" style="background-image: url('../../assets/img/carousel-2.jpg');background-size: cover;">
+                        <div class="carousel-item h-100" style="background-image: url('../../assets/img/sukorejo3.jpg');background-size: cover;">
                             <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
                                 <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
                                     <i class="ni ni-bulb-61 text-dark opacity-10"></i>
                                 </div>
-                                <h5 class="text-white mb-1">Faster way to create web pages</h5>
-                                <p>That’s my skill. I’m not really specifically talented at anything except for the ability to learn.</p>
+                                <h5 class="text-white mb-1">"Kalau tidak mau repot jangan berjuang, karena perjuangan selalu membutuhkan pengorbanan"</h5>
+                                <p>KHR. As'ad Syamsul Arifin</p>
                             </div>
                         </div>
-                        <div class="carousel-item h-100" style="background-image: url('../../assets/img/carousel-3.jpg');background-size: cover;">
+                        <div class="carousel-item h-100" style="background-image: url('../../assets/img/sukorejo2.jpg');background-size: cover;">
                             <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
                                 <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
                                     <i class="ni ni-trophy text-dark opacity-10"></i>
                                 </div>
-                                <h5 class="text-white mb-1">Share with us your design tips!</h5>
-                                <p>Don’t be afraid to be wrong because you can’t learn anything from a compliment.</p>
+                                <h5 class="text-white mb-1">Perjuangan dan pengabdian harus dijunjung tinggi oleh kader Salafiyah Syafi'iyah</h5>
+                                <p>KHR. Ach. Fawa'id As'ad.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item h-100" style="background-image: url('../../assets/img/sukorejo1.jpg');background-size: cover;">
+                            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
+                                <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
+                                    <i class="ni ni-trophy text-dark opacity-10"></i>
+                                </div>
+                                <h5 class="text-white mb-1">"Belajaralah menghargai orang lain! dan belajarlah membalas hinaan dengan senyum manis!"</h5>
+                                <p>KHR. Ahmad Azaim Ibrahimy</p>
                             </div>
                         </div>
                     </div>
